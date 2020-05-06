86067db34c6cba816f2784a89b7fb0e486621c64
Pauline Lee <paulinelcf@163.com>
2020-05-06 14:50:20 +0800

    Add and show Heroes list, and show the selected if exist

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,8 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 1, name: 'Spring' },
+    { id: 2, name: 'Summer' },
+    { id: 3, name: 'Autumn' },
+    { id: 4, name: 'Winter' }
+]
