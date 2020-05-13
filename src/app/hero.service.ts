5083e9b296ddfc96a6900683f941a1dea7e545e1
Pauline Lee <paulinelcf@163.com>
2020-05-13 16:16:02 +0800

    Add ERROR handling for HttpClient

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -24,17 +24,32 @@ export class HeroService {
     this.messageService.add(`HeroService: ${message}`);
   }
 
-  //of(HEROES)替换成HttpClient的get(URL)
   getHeroes(): Observable<Hero[]> {
-    // TODO: send the message _after_ fetching the heroes
-    this.log('fetched heroes');
-    return this.http.get<Hero[]>(this.heroesUrl);
+    //of(HEROES)替换成HttpClient的get(URL)
+    //tap()会查看Observable中的值或者使用那些值做些事情并把它们传出来，但不会改变值本身，
+    //可以用来通过log()往页面底部发送一条信息
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        tap(_ => this.log('fetched heroes')),
+        catchError(this.handleError<Hero[]>('getHeroes',[]))
+      );
   }
 
   getHero(id: number): Observable<Hero> {
-    // TODO: send the message _after_ fetching the hero
-    this.log(`fetched hero id=${id}`);
     //反引号(`)用于定义JavaScript的模板字符串字面量，以便嵌入id
-    return this.http.get<Hero>(`${this.heroesUrl}/${id}`);
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url)
+      .pipe(
+        tap(_ => this.log(`fetched hero id=${id}`)),
+        catchError(this.handleError<Hero>(`getHero id=${id}`)));
+  }
+
+  // 当获取数据失败时，给应用返回一个安全值，让应用继续工作
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      this.log(`${operation} failed: ${error.message}`);
+      return of (result as T); // Let the app keep running by returning an empty result
+    }
   }
 }
